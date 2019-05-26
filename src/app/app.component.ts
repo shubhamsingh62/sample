ce683954b44e586cee950097424afc96577d4dff
shubham <shubham.singh62@gmail.com>
2019-05-26 11:33:28 +0530

    secoand commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'demo';
+
+  public  clicked() {
+     alert("sending message clicked")
+  }
 }
